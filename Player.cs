56065b5ea2794ba68991b51c36b44bf9b8e3d88e
Юрасов Вячеслав <2665603@gmail.com>
2020-02-07 16:04:04 +0500

    Реализованно сохранение данных

--- Player.cs
+++ Player.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.OleDb;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,6 +19,15 @@ namespace GolfScore
         public override string ToString()
         {
             return Name + " " + Money.ToString("N1") + "\r\n";
+        }
+        public void Save() //сохранение итогов
+        {
+            string request = $"INSERT INTO score ([Имя], [Дата], [Сумма])  VALUES ('{Name}', '{DateTime.Now.ToShortDateString()}', '{Money.ToString("N1")}')";
+            OleDbConnection _connection = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0; Data Source=golfData.mdb");
+            _connection.Open();
+            var command = new OleDbCommand(request, _connection);
+            command.ExecuteNonQuery();
+            _connection.Close();
         }
     }
 }
